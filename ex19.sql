e061d8110b73be7009f13951f6b710aeda521bef
Mijosé Difuidi <91575237+Mijosed@users.noreply.github.com>
2024-03-08 01:46:39 +0100

    Update ex19.sql

--- ex19.sql
+++ ex19.sql
@@ -1,24 +1,33 @@
-CREATE TABLE item (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(64),
-    rarity INT,
-    icon_id INT
+-- Création de la table item
+CREATE TABLE item
+(
+    id SERIAL NOT NULL,
+    name VARCHAR(64) NOT NULL DEFAULT '',
+    rarity INT NOT NULL DEFAULT 0,
+    icon_id INT NOT NULL DEFAULT 0,
+
+    PRIMARY KEY (id)
 );
 
+-- Crétion de la table inventory
+CREATE TABLE inventory
+(
+    id SERIAL NOT NULL,
+    item_id INT NOT NULL DEFAULT 0,
+    character_id INT NOT NULL DEFAULT 0,
 
-CREATE TABLE inventory (
-    id SERIAL PRIMARY KEY,
-    item_id INT,
-    character_id INT,
+    PRIMARY KEY (id),
     FOREIGN KEY (item_id) REFERENCES item(id),
     FOREIGN KEY (character_id) REFERENCES character(id)
 );
 
+CREATE TABLE loots
+(
+    id SERIAL NOT NULL,
+    item_id INT NOT NULL DEFAULT 0,
+    creature_gid INT NOT NULL DEFAULT 0,
 
-CREATE TABLE loots (
-    id SERIAL PRIMARY KEY,
-    item_id INT,
-    creature_gid INT,
+    PRIMARY KEY (id),
     FOREIGN KEY (item_id) REFERENCES item(id),
     FOREIGN KEY (creature_gid) REFERENCES creature_template(id)
 );
