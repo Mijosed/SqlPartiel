d3b8f96955da6902fa4a4a7bdb09aff0da9b823b
Mijosé Difuidi <91575237+Mijosed@users.noreply.github.com>
2024-03-08 01:45:50 +0100

    Update ex20.sql

--- ex20.sql
+++ ex20.sql
@@ -1,16 +1,6 @@
-CREATE VIEW v_character_stats AS
-SELECT 
-    ch.name AS character_name,
-    ch.level AS character_level,
-    ch.gold AS gold_earned,
-    (SELECT COUNT(*) FROM character_quests cq WHERE cq.character_id = ch.id AND cq.complete = 1) AS quests_completed,
-    (SELECT COUNT(*) FROM character_quests cq WHERE cq.character_id = ch.id AND cq.complete = 0) AS quests_incomplete,
-    (SELECT COUNT(*) FROM inventory i WHERE i.character_id = ch.id) AS items_in_inventory,
-    (SELECT COUNT(*) FROM creature_kills ck WHERE ck.character_id = ch.id) AS monsters_killed
-FROM 
-    character ch
-ORDER BY 
-    gold_earned DESC,
-    quests_completed DESC,
-    quests_incomplete,
-    items_in_inventory DESC;
+SELECT creature_template.name
+FROM character
+INNER JOIN inventory ON character.id = inventory.character_id
+INNER JOIN loots ON inventory.item_id = loots.item_id
+INNER JOIN creature_template ON loots.creature_gid = creature_template.id
+WHERE character.name = 'LeKrogan';
