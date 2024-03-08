0b6348d06564b09b50603085e660682d11ff3ada
Mijosé Difuidi <91575237+Mijosed@users.noreply.github.com>
2024-03-08 01:49:57 +0100

    Update ex16.sql

--- ex16.sql
+++ ex16.sql
@@ -1,11 +1,18 @@
-CREATE TABLE faction (
-    id SERIAL PRIMARY KEY,
-    color INT,
-    name VARCHAR(64)
+-- Création de la table faction
+CREATE TABLE faction
+(
+    id SERIAL NOT NULL,
+    color INT NOT NULL DEFAULT 0,
+    name VARCHAR(64) NOT NULL DEFAULT '',
+
+    PRIMARY KEY (id)
 );
 
+-- Ajout de contenu dans la table faction
+INSERT INTO faction (id, color, name) 
+VALUES (0, 0x00ff00, 'Enlightened');
 
-INSERT INTO faction (id, color, name) VALUES (0, 0x00FF00, 'Enlightened');
-
-
-ALTER TABLE creature ADD COLUMN faction_id INT REFERENCES faction(id);
+-- Ajout de la colonne faction_id dans la table creature
+ALTER TABLE creature
+ADD COLUMN faction_id INT NOT NULL DEFAULT 0,
+ADD FOREIGN KEY (faction_id) REFERENCES faction(id);
